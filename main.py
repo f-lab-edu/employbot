2f6004886d7188fbaf543cf9c4111bd69f9ad98a
Dongwoo Kang <dongwoomail@icloud.com>
2023-09-11 22:50:41 +0900

    feat : FastAPI 기본코드 작성 및 실행

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello world"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
+    
