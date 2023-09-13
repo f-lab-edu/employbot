0a4506c1e61250f55c926c6685602da1ca215370
Dongwoo Kang <dongwoomail@icloud.com>
2023-09-13 19:01:10 +0900

    feat : Thread 응답 봇 구현

---
+++ SlackAPI.py
@@ -0,0 +1,46 @@
+from slack_sdk import WebClient
+import slack_tokens
+
+class SlackAPI:
+    """
+    Slack API Handler
+    """
+    def __init__(self, token):
+        """
+        ### `WebClient`
+        - `self.client` : `token`(OAuth Token)을 받아 Slack과 상호작용을 허락합니다.
+        """
+        self.client = WebClient(token)
+
+    def get_message(self, channel_id, query):
+        """
+        ### 슬랙 채널 메세지 조회
+        - parameter : `channel_id`, `query`
+        - return : `message_ts`
+        """
+        result = self.client.conversations_history(channel=channel_id)
+        # 채널 내 메세지 정보 리스트
+        messages = result.data['messages']
+        # 채널 내 메세지가 query와 일치하는 메세지 딕셔너리 쿼리
+        message = list(filter(lambda m: m['text'] == query, messages))[0]
+        # 해당 메세지ts를 파싱
+        message_ts = message["ts"]
+        return message_ts
+    
+    def post_comment(self, channel_id, message_ts, text):
+        """
+        ### 슬랙 체널 내 메세지의 Thread에 댓글 달기
+        - parameter : `channel_id`, `message_ts`, `text`
+        - return : `result`
+        """
+        result = self.client.chat_postMessage(
+            channel = channel_id,
+            text = text,
+            thread_ts = message_ts
+        )
+        return result
+
+
+
+
+    
--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
 import uvicorn
+from SlackAPI import *
+import slack_tokens
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello world"}
+@app.post("/")
+async def post_message():
+    query = "테스트입니다."
+    text = "네 확인했습니다."
+    slack_client = SlackAPI(token=slack_tokens.BOT_TOKEN)
+    message_ts = slack_client.get_message(slack_tokens.CHANNEL_ID, query=query)
+    result = slack_client.post_comment(slack_tokens.CHANNEL_ID, message_ts=message_ts, text=text)
+    return result
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
