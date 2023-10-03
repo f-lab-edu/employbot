f6ac22610e1c10ddb8a0ea6759a30724fddbba25
Dongwoo Kang <dongwoomail@icloud.com>
2023-10-03 21:26:03 +0900

    fix : 개발서버 `Request URL` 응답

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 from fastapi import FastAPI, Body
+
+from pydantic.main import BaseModel
 from routers import total_search
 import uvicorn
 
 app = FastAPI()
 app.include_router(total_search.router)
 
-# class SlackModel(BaseModel):
-#     token: str
-#     challenge: str
-#     type: str
+class SlackModel(BaseModel):
+    token: str
+    challenge: str
+    type: str
 
-# @app.post("/")
-# async def post_message(request_body: SlackModel = Body(...)):
-#     response = {"challenge": request_body.challenge}
-#     print(response)
-#     return response
+@app.post("/")
+async def post_message(request_body: SlackModel = Body(...)):
+    response = {"challenge": request_body.challenge}
+    print(response)
+    return response
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
