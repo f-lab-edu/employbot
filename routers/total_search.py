b8bd4628cbf028cffbe88e1bedd76df3d80c26e3
Dongwoo Kang <dongwoomail@icloud.com>
2023-10-11 12:52:05 +0900

    fix : 수정

--- routers/total_search.py
+++ routers/total_search.py
@@ -1,4 +1,24 @@
 from routers import *
+from routers.form_search import search_detail_job
+
+@router.post("/interactive")
+async def post_interactive(request: Request):
+    form_data = await request.form()
+    payload = json.loads(form_data.get("payload"))
+    query=payload["actions"][0]
+    slack_client = SlackAPI(token=SLACK_APP_TOKEN)
+    
+    if query:
+        if query['type'] == "plain_text_input":
+            return
+        if query['type'] == "button":
+            if query['value'] == "total":
+                await search_job() 
+                return
+            if query['value'] == "detail":
+                await search_detail_job() 
+                return
+    return
 
 @router.post("/total")
 async def search_job():
